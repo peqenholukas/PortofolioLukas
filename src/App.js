9028d376653432aeb4b281189282aa8f29c67552
peqenho <lucasotero1844@gmail.com>
2022-09-12 22:11:17 -0300

    Follow the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
+const SocialLinks = () => {
+
+    const links = [
+        {
+            id: 1,
+            child: (
+                <>
+                    Linkedin <FaLinkedin size={30} />
+                </>
+            ),
+            href: "https://www.linkedin.com/in/lucas-otero-7a6692225/",
+            style: "rounded-tr-md"
+        },
+        {
+            id: 2,
+            child: (
+                <>
+                    GitHub <FaGithub size={30} />
+                </>
+            ),
+            href: "https://github.com/peqenholukas?tab=repositories",
+            style: "rounded-tr-md"
+        },
+        {
+            id: 3,
+            child: (
+                <>
+                    Mail <HiOutlineMail size={30} />
+                </>
+            ),
+            href: "mailto:lucasotero1844@gmail.com",
+            style: "rounded-tr-md"
+        },
+        {
+            id: 4,
+            child: (
+                <>
+                    Resume <BsFillPersonLinesFill size={30} />
+                </>
+            ),
+            href: "https://www.youtube.com/",
+            style: "rounded-br-md"
+        }
+    ]
+
+    console.log(links)
+
+    return (
+        <div className='flex flex-col top-[35%] left-0 fixed'>
+            <ul>
+                {
+                    links.map(({ id, child, href, style }) => {
+                        return (
+                            <li
+                                key={id}
+                                className={
+                                    'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + style
+                                }
+                            >
+                                <a href={href} className='flex justify-between items-center w-full text-white'>
+                                    {child}
+                                </a>
+                            </li>
+                        )
+                    })}
+            </ul>
+        </div>
+    )
+}
+
+export default SocialLinks
